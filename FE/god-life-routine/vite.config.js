e9b0d1315cb5d872e6f0538c9a7f1b9403f4c044
조창훈 <cvcvcx9@gmail.com>
2024-09-03 17:21:16 +0900

    Delete: vite-html plugin 남아있던 코드제거

--- FE/god-life-routine/vite.config.js
+++ FE/god-life-routine/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-html'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
