527c62f71e2bcdac4edb3ef0537f851ce37fd972
조창훈 <cvcvcx9@gmail.com>
2024-09-23 13:42:42 +0900

    Chore:모달 임시수정

--- FE/god-life-routine/src/components/Modal.jsx
+++ FE/god-life-routine/src/components/Modal.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Modal = ({children,onClose,showModal}) => {
+    useEffect(()=>{
+        
+    },[showModal])
 if(!showModal) return null;
+const onClickClose = ()=>{
+    console.log("클릭되고있음");
+    onClose(false);
+}
   return (
 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="bg-white p-6 rounded-lg">
         {children}
         <button
-          onClick={onClose}
+          onClick={onClickClose}
           className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
         >
           Close
