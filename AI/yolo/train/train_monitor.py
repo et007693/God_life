5b1409181e52e03bef88f95b8cd05a89e512d2cc
송창용 <thdckdyd123@naver.com>
2024-09-25 15:27:04 +0900

    fix: 경로 오류 -> 상대 경로로 설정

---
+++ AI/yolo/train/train_monitor.py
@@ -0,0 +1,40 @@
+from roboflow import Roboflow
+from ultralytics import YOLO
+import ultralytics
+import yaml
+import os
+import torch
+
+print(torch.cuda.is_available()) 
+ultralytics.checks()
+
+# 현재 실행 중인 스크립트의 디렉토리 경로 가져오기
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 절대 경로 설정: 예를 들어, 'data' 폴더 내 파일 경로
+path = os.path.abspath(os.path.join(current_dir, '..', 'dataset', 'dataset_monitor-1'))
+save_path = os.path.abspath(os.path.join(current_dir, '..', 'model', 'monitor.pt'))
+save_path2 = os.path.abspath(os.path.join(current_dir, '../..', 'flask', 'model', 'monitor.pt'))
+
+if not os.path.exists(path):
+  rf = Roboflow(api_key="pSKVxTiuweLONzMDubjk")
+  project = rf.workspace("novia-putri-o163i").project("dataset_monitor")
+  version = project.version(1)
+  dataset = version.download("yolov8", location=path)  
+
+data = {
+    'test': os.path.join(path, 'test', 'images'),
+    'train': os.path.join(path, 'train', 'images'),
+    'val': os.path.join(path, 'valid', 'images'),
+    'names': ['monitor'],
+    'nc': 1
+}
+
+yaml_file_path = os.path.join(path, 'data.yaml')
+with open(yaml_file_path, 'w') as f:
+    yaml.dump(data, f)
+
+if __name__ == '__main__':
+    model = YOLO('yolov8n.pt')
+    model.train(data=yaml_file_path, epochs=100, patience=50, imgsz=420, batch=32, device='cuda')
+    model.save(save_path)
+    model.save(save_path2)
--- FE/god-life-routine/src/pages/TransferPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TransferPage = () => {
-  return (
-    <div>TransferPage</div>
-  )
-}
-
-export default TransferPage
