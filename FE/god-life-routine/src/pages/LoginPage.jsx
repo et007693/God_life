b4d5f56326afccc02534f88d59119d736bb80066
조창훈 <cvcvcx9@gmail.com>
2024-10-02 15:26:42 +0900

    Feat:팀기상미션구현완료

--- FE/god-life-routine/src/pages/LoginPage.jsx
+++ FE/god-life-routine/src/pages/LoginPage.jsx
@@ -1,29 +1,24 @@
 import React, { useEffect } from 'react'
 import kakaoLogo from '../assets/kakao-logo.png'
 import useUserStore from '../store/useUserStore'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import axiosApi from '../api/axiosApi'
 import { useCookies } from 'react-cookie'
+import useRedirectStore from '../store/useRedirectStore'
 
 const LoginPage = () => {
 // const CLIENT_ID = import.meta.env.VITE_KAKAO_REST_API_KEY
 // const REDIRECT_URI = 'http://localhost:5173/auth/kakao/callback'
 const navigate = useNavigate();
-const {setAccessToken} = useUserStore();
+const location = useLocation();
+const from = location.state?.from?.pathname || "/";
+const {setRedirectUrl} = useRedirectStore();
 
   const onClickKakaoLogin = async () =>{
+    setRedirectUrl(from);
     const baseUrl = import.meta.env.DEV ? "http://localhost:8080" : "http://j11a503.p.ssafy.io"
-    // await setAccessToken("hehehe");
-    // window.location.href = 'http://j11a503.p.ssafy.io:8080/api/v1/oauth2/authorization/kakao'
-    // navigate("/");
     
     window.location.href = `${baseUrl}/api/v1/oauth2/authorization/kakao`
-    // navigate("/")
-    // TODO: 추후 로그인 리다이렉트 수정 및 쿠키 설정 필요
-    // setCookies("accessToken","hehehe");
-    // navigate("/");
-    // window.location.href = 
-    // `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
 
   }
   return (
