b9b72e4105006c3ad816e1ef7abbaf68437dd5dc
cvcvcx9 <cvcvcx9@gmail.com>
2024-10-05 19:12:42 +0900

    Chore:갤럭시24+와 같은 화면에서 폰트사이즈 조절

--- FE/god-life-routine/tailwind.config.js
+++ FE/god-life-routine/tailwind.config.js
@@ -6,6 +6,15 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        'tall': {'raw': '(min-height: 800px)'},
+        'very-tall': {'raw': '(min-height: 1000px)'},
+      },
+      fontSize: {
+        'base': ['16px', '24px'],
+        'lg': ['18px', '28px'],
+        'xl': ['20px', '32px'],
+      },      
       height: {
         'real-screen': 'calc(var(--vh,1vh) * 100)',
       },
